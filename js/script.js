37acb5d5287f05397418edce72628dcd2608f8c2
Steve McNamara <stephenmcnamara36@gmail.com>
2021-01-27 21:10:40 -0700

    updated questions and functions to try and make them work together

--- js/script.js
+++ js/script.js
@@ -6,40 +6,63 @@ var questionTitle = document.querySelector(".questiontitle")
 var questionChoices = document.querySelector(".questionchoices")
 var startButton = document.querySelector(".startbutton")
 var questionsDiv = document.querySelector(".questions")
-var hallOfFame = document.querySelector(".high-scorers")
 var choice1 = document.querySelector("#choice1")
 var choice2 = document.querySelector("#choice2")
 var choice3 = document.querySelector("#choice3")
 var choice4 = document.querySelector("#choice4")
 var timer = document.querySelector(".time-left")
+var hallOfFame = document.querySelector(".hall-of-fame")
+var highScorers = document.querySelector(".high-scorers")
 
 var i = 0
 var questions = [
     //5 questions
     {
-        title: "What is the color of grass?",
-        choices: ["green", "blue", "red", "yellow"],
-        correct: "green"
+        title: "Which of the following is not an HTML attribute?",
+        choices: ["src", "header", "href", "class"],
+        correct: "header"
     },
     {
-        title: "What is the color of the sky?",
-        choices: ["green", "blue", "red", "yellow"],
-        correct: "blue"
+        title: "What is used to store information in JavaScript?",
+        choices: ["variable", "loop", "function", "method"],
+        correct: "method"
+    },
+    {
+        title: "How many parameters are needed for the setInterval() method?",
+        choices: ["3", "10", "2", "5"],
+        correct: "2"
+    },
+    {
+        title: "The code that is executed in a function is contained within the:",
+        choices: ["parentheses", "curly brackets","quotation marks","brackets"],
+        correct: "curly brackets"
+    },
+    {
+        title: "Which of these is not part of a for loop?",
+        choices: ["counter", "condition", "iterator", "calculator"]
     }
 ]
 
 function startQuiz () {
     startPage.setAttribute("class", "hide")
+    //hide the startPage with CSS attribute
     questionsDiv.removeAttribute("class", "hide")
+    // show questions div by removing hide attribute
     startTimer()
-    displayQuestion()
+    //execute startTimer function
+    displayQuestion(i)
+    //execute displayQuestion()
 }
 
-startButton.addEventListener("click", startQuiz)
 
 
-function displayQuestion() {
-    if (questions[i] < questions.length) {
+
+
+function displayQuestion(i) {
+    //if question index is less than length of questions,
+    //display the text content of the questions[i]. title on page
+    //display the text of the answer choices in button elements
+    for (questions[i]; i < questions.length; i++) {
         questionTitle.textContent = questions[i].title
         choice1.textContent = questions[i].choices[0] 
         choice2.textContent = questions[i].choices[1] 
@@ -50,49 +73,56 @@ function displayQuestion() {
         choice2.addEventListener("click", checkAnswer)
         choice3.addEventListener("click", checkAnswer)
         choice4.addEventListener("click", checkAnswer)
-    } else {
-        endQuiz()
-    }}
+       }
+    } 
+
 
 function checkAnswer(answer) {
-    if (answer === questions[i].correct) {
+    if (answer === questions.correct) {
         i++
         displayQuestion()
     } else {
         timeRemaining = timeRemaining - 10
         i++
         displayQuestion()
-    }
-    //this will check the answer
-    //will need i++ to increment questions 
-    //then call display question
+    } 
 }              
 
 function startTimer() {
-     var time = setInterval(function () {
+    //start timer with setInterval
+     var countdown = setInterval(function () {
+         //while time is remaining, continue counting down
         if (timeRemaining > 0) {
             timer.textContent = "Time Remaining: " + timeRemaining;
             timeRemaining--
-        } else {clearInterval(time);
-        endQuiz()}
+        }   if (timeRemaining === 0) { 
+            clearInterval(countdown);
+            timer.setAttribute("class", "hide")
+            endQuiz();
+        }
     }, 1000)
 }
 
 
-function endQuiz () {
+function endQuiz() {
+    score === timeRemaining
     questionsDiv.setAttribute("class", "hide")
     hallOfFame.removeAttribute("class", "hide")
-    score == timeRemaining
-    var highScorerInput = prompt("Please enter your initials:")
-    var highScore = {
-        initials: highScorerInput,
-        score: score,
-    }
-    highScoreStore = localStorage.setItem("high-score", JSON.stringify (highScore))
-   
+    alert("Your score was " + score + "!")
+    // displayHOF()
 }
 
-function displayHOF () {
-   var highScorers = localStorage.getItem(JSON.parse("highScoreStore"))
-   hallOfFame.textContent = highScorers
-}
+// function displayHOF () {
+//     score === timeRemaining
+//     var highScorerInput = prompt("Please enter your initials:")
+//     var highScore = {
+//         initials: highScorerInput,
+//         score: score,
+//     }
+//     highScoreStore = localStorage.setItem("high-score", JSON.stringify("highScore"))
+//     var leaderBoard = localStorage.getItem(JSON.parse("highScoreStore"))
+//     highScorers.textContent = leaderBoard
+// }
+
+//click on the start button which executes the startQuiz function
+startButton.addEventListener("click", startQuiz)
